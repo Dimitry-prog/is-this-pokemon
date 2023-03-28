388ef17a17eaebd9603557f7d3fd22af125edd3e
Dimitry-prog <kfifa3@gmail.com>
2023-03-28 22:27:34 +0300

    fix: search route

--- app/api/search/route.ts
+++ app/api/search/route.ts
@@ -1,10 +1,12 @@
 import {NextResponse} from "next/server";
 import pokemon from '@/pokemon.json';
 
-export const GET = (req: Request) => {
+export const GET = async (req: Request) => {
   const {searchParams} = new URL(req.url);
   const name = searchParams.get("name");
-  const pokemonData = pokemon.filter(pok => pok.name.toLowerCase().includes(name?.toLowerCase()) ?? "");
+  const pokemonData = pokemon.filter(pok =>
+    pok.name.toLowerCase().includes(name?.toLowerCase() ?? "")
+  );
 
   return NextResponse.json(pokemonData.slice(0, 10));
 }
