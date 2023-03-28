1591d8267cd20de60f1de778a6294d9d7017fe49
Dimitry-prog <kfifa3@gmail.com>
2023-03-28 23:34:27 +0300

    fix: Home

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,16 @@
 import Image from 'next/image'
 import {Inter} from 'next/font/google'
+import PokemonList from "@/app/components/PokemonList";
 
 const inter = Inter({subsets: ['latin']})
 
 export default async function Home() {
-  const req = await fetch("http://localhost:3000/api/search?name=Bulbasaur");
+  const req = await fetch("http://localhost:3001/api/search");
   const data = await req.json();
 
   return (
     <main className={inter.className}>
-      {JSON.stringify(data)}
+      <PokemonList pokemons={data}/>
     </main>
   )
 }
